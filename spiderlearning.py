0aff5b501b4c9196e2c6071979394cf063f6b9ec
单者无惧 <1668926294@qq.com>
2018-01-19 21:43:32 +0800

    爬虫初步

--- spiderlearning.py
+++ spiderlearning.py
@@ -1,5 +1,16 @@
 #!usr/bin/python
 import urllib.request
-url = "http://www.baidu.com"
-get = urllib.request.urlopen(url).read()
-print(get)
+from bs4 import BeautifulSoup
+url = "https://www.douban.com/"
+page = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(page)
+print(soup.prettify())
+i = 0
+for link in soup.find_all('a'):
+    print(i, link.get('href'))
+    i += 1
+
+
+
+
+
