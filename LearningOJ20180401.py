e853ba4bb4eef65c6ec086745ba223d64d2097b6
单者无惧 <1668926294@qq.com>
2018-04-03 20:31:26 +0800

    python 练习

--- LearningOJ20180401.py
+++ LearningOJ20180401.py
@@ -9,4 +9,21 @@ def unique_in_order(iterable):
     return yh
 
 
-print(unique_in_order("AAABBBCCCGGTTTC"))
+def duplicate_encode(word):
+    word = word.lower()
+    dict = {}
+    for c in word:
+        if c in dict.keys():
+           dict[c] += 1
+        else:
+           dict[c] = 1
+    sen = ""
+    for number in word:
+       if dict[number] == 1:
+           sen += "("
+       else:
+           sen += ")"
+    return sen
+
+
+print(duplicate_encode("Success"))
