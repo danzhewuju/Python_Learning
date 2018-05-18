5d4fb3c7a95f68172781961bb67b90011d28c299
alexanderdan <1668926294@qq.com>
2018-05-18 09:36:25 +0800

    网易云爬虫

--- spiderlearning.py
+++ spiderlearning.py
@@ -1,26 +1,9 @@
 #!usr/bin/python3
 import urllib.request
 from bs4 import BeautifulSoup
-url = "https://www.douban.com/"
-page = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(page)
-print(soup.prettify())
-f = open("douban.txt", "w+")
-i = 1
-
-
-for link in soup.find_all('a'):
-    s = link.get('href')
-    if s!=None:
-        f.write(str(i) +"\t")
-        f.write(s)
-        f.write("\n")
-        i += 1
-    print(i, s)
-
-
+f= open("后来.txt", 'a+')
+f.write("*************************")
 f.close()
-
 
 
 
